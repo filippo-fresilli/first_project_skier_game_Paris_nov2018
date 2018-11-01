1eade2072b2a7c6a3f8fc75050a78f71154b3bc5
filippo fresilli <illiserf@gmail.com>
2018-11-01 18:10:24 +0100

    more bugs fixed and new features

--- js/script.js
+++ js/script.js
@@ -48,7 +48,23 @@ class Obstacle {
         oCtx.drawImage(this.image, this.x, this.y, this.width, this.height)
     }
 
+    flagDraw() {
+
+        if(!skier.isCrashed){
+
+            this.y -= this.speedY;
+            this.x -= this.speedX; 
+            if(this.y < -this.height){
+                this.y = 1000;    
+            }
+        }
+
+        oCtx.drawImage(this.image, this.x, this.y, this.width, this.height)
+    }
+
 }
+    
+
 
 //CREATING SKIER
 var skierImg = new Image();
@@ -73,17 +89,22 @@ function changeSpeed(speedX,speedY) {
         allObstacles[i].speedX = speedX;
         allObstacles[i].speedY = speedY;
     } 
+
+    for (var i = 0; i < flags.length; i++){
+        flags[i].speedX = speedX;
+        flags[i].speedY = speedY;
+    } 
+
     for (var i = 0; i < horObstacles.length; i++){
         horObstacles[i].speedX = speedX;
         horObstacles[i].speedY = speedY;
     } 
 
+
 }
 
 
 //CREATING NEW OBSTACLES
-var bgImg = new Image();
-bgImg.src = "./images/Artboard.png";
 
 var treeImg = new Image();
 treeImg.src = "./images/evergreen-tree.png";
@@ -108,20 +129,6 @@ var horObstacles =[
 var allObstacles = [
     //          (img    , x, y, wi, hei)
     // tree osbstacles right
-    new Obstacle(treeImg, 0, 0, 35, 40),
-    new Obstacle(treeImg, 0,  80, 35, 40),
-    new Obstacle(treeImg, 0, 160, 35, 40),
-    new Obstacle(treeImg, 0, 240, 35, 40),
-    new Obstacle(treeImg, 0, 320, 35, 40),
-    new Obstacle(treeImg, 0, 400, 35, 40),
-    new Obstacle(treeImg, 0, 480, 35, 40),
-    new Obstacle(treeImg, 0, 560, 35, 40 ),
-    new Obstacle(treeImg, 0, 640, 35, 40),
-    new Obstacle(treeImg, 0, 720, 35, 40),
-    new Obstacle(treeImg, 0, 800, 35, 40),
-    new Obstacle(treeImg, 0, 880, 35, 40),
-    new Obstacle(treeImg, 0, 960, 35, 40),
-
     new Obstacle(treeImg, -35, 40, 35, 40 ),
     new Obstacle(treeImg, -35, 120, 35, 40),
     new Obstacle(treeImg, -35, 200, 35, 40),
@@ -129,7 +136,7 @@ var allObstacles = [
     new Obstacle(treeImg, -35, 360, 35, 40),
     new Obstacle(treeImg, -35, 440, 35, 40),
     new Obstacle(treeImg, -35, 520, 35, 40),
-    new Obstacle(treeImg, -35, 600, 35, 40 ),
+    new Obstacle(treeImg, -35, 600, 35, 40),
     new Obstacle(treeImg, -35, 680, 35, 40),
     new Obstacle(treeImg, -35, 760, 35, 40),
     new Obstacle(treeImg, -35, 840, 35, 40),
@@ -144,7 +151,7 @@ var allObstacles = [
     new Obstacle(treeImg, -75, 320, 35, 40),
     new Obstacle(treeImg, -75, 400, 35, 40),
     new Obstacle(treeImg, -75, 480, 35, 40),
-    new Obstacle(treeImg, -75, 560, 35, 40 ),
+    new Obstacle(treeImg, -75, 560, 35, 40),
     new Obstacle(treeImg, -75, 640, 35, 40),
     new Obstacle(treeImg, -75, 720, 35, 40),
     new Obstacle(treeImg, -75, 800, 35, 40),
@@ -158,7 +165,7 @@ var allObstacles = [
     new Obstacle(treeImg, -100, 360, 35, 40),
     new Obstacle(treeImg, -100, 440, 35, 40),
     new Obstacle(treeImg, -100, 520, 35, 40),
-    new Obstacle(treeImg, -100, 600, 35, 40 ),
+    new Obstacle(treeImg, -100, 600, 35, 40),
     new Obstacle(treeImg, -100, 680, 35, 40),
     new Obstacle(treeImg, -100, 760, 35, 40),
     new Obstacle(treeImg, -100, 840, 35, 40),
@@ -166,21 +173,8 @@ var allObstacles = [
     new Obstacle(treeImg, -100, 1000, 35, 40),
     new Obstacle(treeImg, -100, 1080, 35, 40),
     
+        //          (img    , x, y, wi, hei)
     // tree osbstacles left
-    new Obstacle(treeImg, 1465, 0, 35, 40),
-    new Obstacle(treeImg, 1465,  80, 35, 40),
-    new Obstacle(treeImg, 1465, 160, 35, 40),
-    new Obstacle(treeImg, 1465, 240, 35, 40),
-    new Obstacle(treeImg, 1465, 320, 35, 40),
-    new Obstacle(treeImg, 1465, 400, 35, 40),
-    new Obstacle(treeImg, 1465, 480, 35, 40),
-    new Obstacle(treeImg, 1465, 560, 35, 40 ),
-    new Obstacle(treeImg, 1465, 640, 35, 40),
-    new Obstacle(treeImg, 1465, 720, 35, 40),
-    new Obstacle(treeImg, 1465, 800, 35, 40),
-    new Obstacle(treeImg, 1465, 880, 35, 40),
-    new Obstacle(treeImg, 1465, 960, 35, 40),
-
     new Obstacle(treeImg, 1500, 40, 35, 40 ),
     new Obstacle(treeImg, 1500, 120, 35, 40),
     new Obstacle(treeImg, 1500, 200, 35, 40),
@@ -188,7 +182,7 @@ var allObstacles = [
     new Obstacle(treeImg, 1500, 360, 35, 40),
     new Obstacle(treeImg, 1500, 440, 35, 40),
     new Obstacle(treeImg, 1500, 520, 35, 40),
-    new Obstacle(treeImg, 1500, 600, 35, 40 ),
+    new Obstacle(treeImg, 1500, 600, 35, 40),
     new Obstacle(treeImg, 1500, 680, 35, 40),
     new Obstacle(treeImg, 1500, 760, 35, 40),
     new Obstacle(treeImg, 1500, 840, 35, 40),
@@ -203,39 +197,63 @@ var allObstacles = [
     new Obstacle(treeImg, 1535, 320, 35, 40),
     new Obstacle(treeImg, 1535, 400, 35, 40),
     new Obstacle(treeImg, 1535, 480, 35, 40),
-    new Obstacle(treeImg, 1535, 560, 35, 40 ),
+    new Obstacle(treeImg, 1535, 560, 35, 40),
     new Obstacle(treeImg, 1535, 640, 35, 40),
     new Obstacle(treeImg, 1535, 720, 35, 40),
     new Obstacle(treeImg, 1535, 800, 35, 40),
     new Obstacle(treeImg, 1535, 880, 35, 40),
     new Obstacle(treeImg, 1535, 960, 35, 40),
 
+    new Obstacle(treeImg, 1570, 40, 35, 40 ),
+    new Obstacle(treeImg, 1570, 120, 35, 40),
+    new Obstacle(treeImg, 1570, 200, 35, 40),
+    new Obstacle(treeImg, 1570, 280, 35, 40),
+    new Obstacle(treeImg, 1570, 360, 35, 40),
+    new Obstacle(treeImg, 1570, 440, 35, 40),
+    new Obstacle(treeImg, 1570, 520, 35, 40),
+    new Obstacle(treeImg, 1570, 600, 35, 40),
+    new Obstacle(treeImg, 1570, 680, 35, 40),
+    new Obstacle(treeImg, 1570, 760, 35, 40),
+    new Obstacle(treeImg, 1570, 840, 35, 40),
+    new Obstacle(treeImg, 1570, 920, 35, 40),
+    new Obstacle(treeImg, 1570, 1000, 35, 40),
+    new Obstacle(treeImg, 1570, 1080, 35, 40),
+
+        //          (img    , x, y, wi, hei)
     new Obstacle(treeImg, 333, 40, 35, 40 ),
-    new Obstacle(treeImg, 444, 120, 35, 40),
+    new Obstacle(treeImg, 1340, 120, 35, 40),
     new Obstacle(treeImg, 859, 200, 35, 40),
-    new Obstacle(treeImg, 600, 280, 35, 40),
+    new Obstacle(treeImg, 350, 280, 35, 40),
     new Obstacle(treeImg, 1200, 360, 35, 40),
     new Obstacle(treeImg, 678, 440, 35, 40),
     new Obstacle(treeImg, 890, 520, 35, 40),
 
+    new Obstacle(treeImg, 50, 900, 35, 40 ),
     new Obstacle(treeImg, 100, 450, 35, 40 ),
     new Obstacle(treeImg, 500, 300, 35, 40),
     new Obstacle(treeImg, 600, 400, 35, 40),
     new Obstacle(treeImg, 810,300, 35, 40),
     new Obstacle(treeImg, 120, 120, 35, 40),
+    new Obstacle(treeImg, 570, 800, 35, 40),
+    new Obstacle(treeImg, 1360, 900, 35, 40),
+    new Obstacle(treeImg, 1190,1000, 35, 40),
+    new Obstacle(treeImg, 1200, 451, 35, 40),
 
-    new Obstacle(snowManImg, 222, 300, 35, 40),
-    new Obstacle(snowManImg, 822, 1420, 35, 40),
-    new Obstacle(snowManImg, 1000, 1230, 35, 40),
-    new Obstacle(snowManImg, 1500, 1000, 35, 40),
+    new Obstacle(snowManImg, 222, 300, 25, 28),
+    new Obstacle(snowManImg, 822, 1420, 25, 28),
+    new Obstacle(snowManImg, 1000, 1230, 25, 28),
+    new Obstacle(snowManImg, 1110, 1000, 25, 28),
 ];
 
 var flagImg = new Image();
 flagImg.src = "./images/bandiera.png";
 
 var flags = [
-    new Obstacle(flagImg, 400, 750, 35, 40),
-    new Obstacle(flagImg, 500, 750, 35, 40),
+    new Obstacle(flagImg, 400, 500, 30, 35),
+    new Obstacle(flagImg, 500, 500, 30, 35),
+
+    new Obstacle(flagImg, 1300, 1650, 30, 35),
+    new Obstacle(flagImg, 1400, 1650, 30, 35),
 ];
 
 
@@ -245,14 +263,27 @@ var gameOver = {
         this.opacity += 0.01;
         oCtx.globalAlpha = this.opacity;
         oCtx.font = "bold 70px monospace"
+
+        var size = oCtx.measureText("Game Over");
+        var gameOverX = (obstacleCanvas.width / 2) - (size.width / 2);
     
         oCtx.fillStyle = "tomato";
-        oCtx.fillText("Game Over", 700, 300);
-    
+        oCtx.fillText("Game Over", gameOverX, 300); // can I write "height/2"?
+
         oCtx.lineWidth = 3;
-        oCtx.strokeText("Game Over", 700, 300);
+        oCtx.strokeText("Game Over", gameOverX, 300);
+        oCtx.font = "40px monospace"
+        var size = oCtx.measureText("Press cmd + R or control + R to start regain");
+        oCtx.fillText(
+            "Press cmd + R or control + R to start regain",
+            (obstacleCanvas.width / 2) - (size.width / 2) ,
+            390
+        );
+    
     
         oCtx.globalAlpha = 1;
+
+        // INSERT BUTTON TO Start From SKretch
     }
 };
 
@@ -270,7 +301,7 @@ document.onkeydown = function () {
         changeSpeed(0,0);
         if(skier.direction === "down"){
             skier.direction = "half-left";
-            changeSpeed(-4,3);
+            changeSpeed(-4,2);
             skierImg.src = "./images/skier_half_left.png";
             
         } else if (skier.direction === "half-left"){
@@ -283,10 +314,24 @@ document.onkeydown = function () {
             changeSpeed(0,0);
             skier.x -= 10;
             skierImg.src = "./images/skier_left.png";
-        };
+
+        } else if (skier.direction === "right") {
+            skier.direction = "half-right";
+            // changeSpeed(x,y)
+            changeSpeed(4,2);
+            skierImg.src = "./images/skier_half_right.png";
+
+        } else if (skier.direction === "half-right") {
+            skier.direction = "down";
+            // changeSpeed(x,y)
+            changeSpeed(0,5);
+            skierImg.src = "./images/skier_down.png";
+        }
         break;
 
         case 38:  // up arrow
+
+        //fix bug here
         event.preventDefault();
         skier.y -= 10;
         break;
@@ -296,7 +341,7 @@ document.onkeydown = function () {
         if(skier.direction === "down"){
             skier.direction = "half-right";
             // changeSpeed(x,y)
-            changeSpeed(4,3);
+            changeSpeed(4,2);
             skierImg.src = "./images/skier_half_right.png";
             
         } else if (skier.direction === "half-right"){
@@ -310,8 +355,16 @@ document.onkeydown = function () {
             skier.x -= -10;
             skierImg.src = "./images/skier_right.png";
 
-        };
+        } else if (skier.direction === "left") {
+            skier.direction = "half-left";
+            changeSpeed(-4,2);
+            skierImg.src = "./images/skier_half_left.png";
 
+        } else if (skier.direction === "half-left") {
+            skier.direction = "down";
+            changeSpeed(0,5);
+            skierImg.src = "./images/skier_down.png";
+        }
         break;
 
         case 40:  // down arrow
@@ -375,7 +428,22 @@ function drawEverything() {
         if(skier.isCrashed){
             gameOver.drawMe();
         }
+    });  
+
+    flags.forEach(function(oneFlag){
+                    
+            oneFlag.flagDraw();
+            
+            if(rectangleCollision(skier, oneFlag)){
+                // skier.isCrashed = true;
+                // oneFlag.isCrashed = true;
+            }
+            
+        if(skier.isCrashed){
+            gameOver.drawMe();
+        }
     });
+
 }
 
 
